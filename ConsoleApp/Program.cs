49106942c92b8a19eedee97ef37753293b4b6354
marcocunhasilva <marcocunhasilvadeveloper@gmail.com>
2022-07-09 22:37:45 -0300

    Descrição dos Tipos Primitivos em C#.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -15,29 +15,52 @@ namespace ConsoleApp
             Console.ReadKey();
 
             // Tipos primitivos do C#
-            bool bCondition;    // Represanta valores lógicos: true ou false;
+            bool bCondition;    // Representa valores lógicos: true ou false;
             char cOneCharacter; // Representa um caractere como uma unidade de código UTF-16;
             byte nByte;         // Representa um número inteiro sem sinal de 8 bits, no intervalo
                                 // entre 0 a 255;
-            uint;               // Representa um número inteiro sem sinal de 32 bits, no intervalo
+            uint nUint;         // Representa um número inteiro sem sinal de 32 bits, no intervalo
                                 // entre 0 a 4.294.967.295;
-            int;                // Representa um número inteiro com sinal de 32 bits, no intervalo
+            int nInt;           // Representa um número inteiro com sinal de 32 bits, no intervalo
                                 // entre -2.147.483.648 a 2.147.483.647;
-            long;               // Representa um número inteiro com sinal de 64 bits, no intervalo
+            long nLong;         // Representa um número inteiro com sinal de 64 bits, no intervalo
                                 // entre -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807;
-            ulong;              // Representa um número inteiro sem sinal de 64 bits, no intervalo
+            ulong nULong;       // Representa um número inteiro sem sinal de 64 bits, no intervalo
                                 // entre 0 a 18.446.744.073.709.551.615.
-            float;              // Representa um número de ponto flutuante de 4 bytes, no intervalo
+            float nFloat;       // Representa um número de ponto flutuante de 4 bytes, no intervalo
                                 // entre ±1,5 x 10e−45 e ±3,4 x 10e38, com precisão aproximada          
-                                // entre 6 a 9 dígitos;         
-            double;             // Representa um número de ponto flutuante de 8 bytes, no intervalo
+                                // entre 6 a 9 dígitos;
+            double nDouble;     // Representa um número de ponto flutuante de 8 bytes, no intervalo
                                 // entre ±5.0 × 10e−324 e ±1.7e10308, com precisão aproximada 
                                 // entre 15 a 17 dígitos (utilizado para cálculos de VALORES CIENTÍFICOS);
-            decimal;            // Representa um número de ponto flutuante de 16 bytes, no intervalo
+            decimal nDecimal;   // Representa um número de ponto flutuante de 16 bytes, no intervalo
                                 // entre ±1.0 × 10e−28 e ±7,9228 x 10e28, com precisão aproximada 
                                 // entre 28 e 29 dígitos (utilizado em VALORES MONETÁRIOS);
-            string;             // Representa textos como uma sequencia de unidades de código UTF-16.
+            string cString;     // Representa textos como uma sequencia de unidades de código UTF-16.
 
+            Console.WriteLine('// Tipos primitivos do C#');
+            Console.WriteLine('bool bCondition;    // Representa valores lógicos: true ou false;');
+            Console.WriteLine('char cOneCharacter; // Representa um caractere como uma unidade de código UTF-16;');
+            Console.WriteLine('byte nByte;         // Representa um número inteiro sem sinal de 8 bits, no intervalo');
+            Console.WriteLine('                    // entre 0 a 255;');
+            Console.WriteLine('uint nUint;         // Representa um número inteiro sem sinal de 32 bits, no intervalo');
+            Console.WriteLine('                    // entre 0 a 4.294.967.295;');
+            Console.WriteLine('int nInt;           // Representa um número inteiro com sinal de 32 bits, no intervalo');
+            Console.WriteLine('                    // entre -2.147.483.648 a 2.147.483.647;');
+            Console.WriteLine('long nLong;         // Representa um número inteiro com sinal de 64 bits, no intervalo');
+            Console.WriteLine('                    // entre -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807;');
+            Console.WriteLine('ulong nULong;       // Representa um número inteiro sem sinal de 64 bits, no intervalo');
+            Console.WriteLine('                    // entre 0 a 18.446.744.073.709.551.615.');
+            Console.WriteLine('float nFloat;       // Representa um número de ponto flutuante de 4 bytes, no intervalo');
+            Console.WriteLine('                    // entre ±1,5 x 10e−45 e ±3,4 x 10e38, com precisão aproximada          ');
+            Console.WriteLine('                    // entre 6 a 9 dígitos;');
+            Console.WriteLine('double nDouble;     // Representa um número de ponto flutuante de 8 bytes, no intervalo');
+            Console.WriteLine('                    // entre ±5.0 × 10e−324 e ±1.7e10308, com precisão aproximada ');
+            Console.WriteLine('                    // entre 15 a 17 dígitos (utilizado para cálculos de VALORES CIENTÍFICOS);');
+            Console.WriteLine('decimal nDecimal;   // Representa um número de ponto flutuante de 16 bytes, no intervalo');
+            Console.WriteLine('                    // entre ±1.0 × 10e−28 e ±7,9228 x 10e28, com precisão aproximada ');
+            Console.WriteLine('                    // entre 28 e 29 dígitos (utilizado em VALORES MONETÁRIOS);');
+            Console.WriteLine('string cString;     // Representa textos como uma sequencia de unidades de código UTF-16.');
 
         }
     }
